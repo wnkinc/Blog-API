d91d3586738fe7d1e38b06a59d0735ee98b7a7c8
wnkinc <140204064+wnkinc@users.noreply.github.com>
2025-01-17 00:15:18 -0800

    Add and test login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,9 @@
+// routes/auth.routes.js
 const express = require("express");
-const { signup } = require("../controllers/auth.controller");
+const { signup, login } = require("../controllers/auth.controller");
 const router = express.Router();
 
-router.post("/login" /* Controller Logic */);
+router.post("/login", login);
 
 router.post("/signup", signup);
 
