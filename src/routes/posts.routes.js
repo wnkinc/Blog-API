a58a83a8897789fcdabcb037e6718122223da788
wnkinc <140204064+wnkinc@users.noreply.github.com>
2025-01-17 20:27:12 -0800

    Add and test PUT post

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,13 +3,14 @@ const {
   getAllPosts,
   getPostBySlug,
   createPost,
+  updatePost,
 } = require("../controllers/posts.controller");
 const router = express.Router();
 
 router.get("/", getAllPosts); // Fetch all posts
 router.get("/:slug", getPostBySlug); // Fetch single post by slug
 router.post("/", createPost);
-router.put("/:id" /* Update a post */);
+router.put("/:id", updatePost);
 router.delete("/:id" /* Delete a post */);
 
 module.exports = router;
